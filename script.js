c16c722b21470cad0d0202b29b3f19e28af62a21
brav747 <ricky.richmoon@gmail.com>
2025-03-20 08:09:48 +0100

    styles

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const bubble = document.querySelector('.bubble');
-const container = document.querySelector('.container');
+document.addEventListener("DOMContentLoaded", function() {
+    const bubble = document.querySelector(".bubble");
+    let isActive = false;
 
-bubble.addEventListener('click', () => {
-    container.classList.toggle('active');
+    bubble.addEventListener("click", function() {
+        isActive = !isActive;
+        if (isActive) {
+            bubble.style.backgroundImage = "url('images/Background-on.png')";
+            bubble.style.boxShadow = "0 0 50px 30px rgba(255, 255, 0, 0.8)";
+        } else {
+            bubble.style.backgroundImage = "url('images/Background-off.png')";
+            bubble.style.boxShadow = "none";
+        }
+    });
 });
